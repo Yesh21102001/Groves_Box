b8136c359a276e32f8cd06f51aec39268cb3904b
Yeswanth Kumar <yeswanthkona021@gmail.com>
2026-02-11 19:00:06 +0530

    first commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -852,17 +852,24 @@ export default function HomePage() {
       {totalItems > 0 && (
         <div
           className="
-    fixed left-0 right-0
-    bg-white border-t border-gray-200 shadow-lg z-50
+      fixed z-50
+      bg-[#F0F4F1] border-t border-gray-200 shadow-lg
 
-    bottom-[70px] left-3 right-3       /* mobile */
-    sm:bottom-0              /* desktop */
+      bottom-[70px] left-3 right-3          /* mobile */
+      
+     sm:bottom-0 
+sm:left-1/2 
+sm:-translate-x-1/2 
+sm:w-[500px] 
+sm:rounded-t-[20px] 
+sm:rounded-b-none
+                    /* desktop width */
 
-    p-5 rounded-[20px]
-    sm:p-4 sm:rounded-none bg-[#F0F4F1]
-  "
+      p-5 rounded-[20px]
+      sm:p-4 sm:rounded-[16px]
+    "
         >
-          <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
               <div className="bg-[#244033] text-white rounded-full w-8 h-8 flex items-center justify-center text-sm font-medium">
                 {totalItems}
@@ -884,6 +891,8 @@ export default function HomePage() {
           </div>
         </div>
       )}
+
+
 
 
     </div>

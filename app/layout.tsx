3d74a64f492260d4f1480d8498fcc89016dec113
Yeswanth Kumar <yeswanthkona021@gmail.com>
2026-02-20 12:28:32 +0530

    tenth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning={true}
         className={`${dmSans.variable} ${inter.variable} antialiased`}
       >
         <Initializer />
